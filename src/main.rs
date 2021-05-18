94a342abf8044e28a6b1d5d69879f0165c3309d6
Yannik Sander <me@ysndr.de>
2021-05-18 23:06:30 +0200

    Configure log level

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
 use doorman::{manager::Manager, registry::Registry};
 use doorman::interfaces::services::Registry as RegistryTrait;
 use simple::{actuator, authenticator, device::SimpleDevice};
+use clap::Clap;
 
 mod simple;
 
+#[derive(Clap, Debug, Clone)]
+#[clap()]
+struct Args {
+
+
+    /// How much logging to enable
+    /// -v:    warn
+    /// -vv:   info
+    /// -vvv:  debug
+    /// -vvvv: trace
+    /// default: error
+    #[clap(short, parse(from_occurrences))]
+    verbosity: u8,
+
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    dotenv::dotenv().ok();
+
+    let args = Args::parse();
+
+
+    env_logger::Builder::from_default_env()
+    .filter_level(match args.verbosity {
+        0 => LevelFilter::Error,
+        1 => LevelFilter::Warn,
+        2 => LevelFilter::Info,
+        3 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
+    })
+    .init();
 
     let mut registry = Registry::new();
     registry.register_device(SimpleDevice("OnePlus 5".to_string()))?;
